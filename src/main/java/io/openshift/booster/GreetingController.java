831ca1c81d90645f1be0b00e48a8429a4daebf3d
test <test@AMB00806.local>
2019-02-26 15:42:21 +0530

    updated spring security keycloak

--- src/main/java/io/openshift/booster/GreetingController.java
+++ src/main/java/io/openshift/booster/GreetingController.java
@@ -4,6 +4,9 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
 
 @RestController
 public class GreetingController {
