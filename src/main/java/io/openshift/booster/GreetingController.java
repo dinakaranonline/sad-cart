7aa86ff06e95a769065b33235adeb95947dffb0c
test <test@AMB00806.local>
2019-02-26 15:08:51 +0530

    Spring Security included

--- src/main/java/io/openshift/booster/GreetingController.java
+++ src/main/java/io/openshift/booster/GreetingController.java
@@ -11,9 +11,15 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/greeting1")
     public Greeting1 greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting1(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    @GetMapping(path = "/logout")
+	public String logout(HttpServletRequest request) throws ServletException {
+		request.logout();
+		return "/";
+	}
 }
