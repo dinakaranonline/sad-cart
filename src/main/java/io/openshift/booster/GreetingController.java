0e7f80ee39553c114535caa461790ce5fc86e2dc
test <test@AMB00806.local>
2019-02-26 14:39:38 +0530

    Added controller

---
+++ src/main/java/io/openshift/booster/GreetingController.java
@@ -0,0 +1,19 @@
+package io.openshift.booster;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting1 greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting1(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
